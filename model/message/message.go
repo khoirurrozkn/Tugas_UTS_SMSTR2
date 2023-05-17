87511ff544a35d8ee7d53c3d72692413d63c179b
mgendyap <130473129+mgendyap@users.noreply.github.com>
2023-05-18 00:28:29 +0700

    Update message.go
    membuat massage untuk mencari pesan didalam program

--- model/message/message.go
+++ model/message/message.go
@@ -3,3 +3,19 @@ package messageModel
 import (
 	"utsstrukdat/db"
 )
+
+
+func FindMessage(user string) *[]db.FieldMessage {
+	var data_Message *db.Message = &db.DataMessage
+	temp := data_Message.Next
+
+	var respons []db.FieldMessage
+	for temp != nil {
+		if temp.Data.To == user {
+
+			respons = append(respons, temp.Data)
+		}
+		temp = temp.Next
+	}
+	return &respons
+}
