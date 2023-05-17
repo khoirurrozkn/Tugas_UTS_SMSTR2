f0372b55a9eadf975c4a9de80102faf83f7eaa27
khoirurrozkn <irurkampus@gmail.com>
2023-05-17 21:37:35 +0700

    Add files via upload

---
+++ controller/user/user.go
@@ -0,0 +1,56 @@
+package userController
+
+import (
+	"utsstrukdat/db"
+	"utsstrukdat/model/user"
+)
+
+func Register(req db.FieldUser, verPassword string) int {
+
+	check := userModel.FindOne(req.Username)
+
+	if check.Username != "" {
+		return 409
+	}
+	
+	if req.Password != verPassword {
+		return 400
+	}
+
+	userModel.Create(req)
+	return 200
+}
+
+func Login(req db.FieldUser) db.FieldUser {
+
+	check := userModel.FindOne(req.Username)
+
+	if check.Username == req.Username && check.Password == req.Password {
+		return check
+	}
+
+	return db.FieldUser{}
+}
+
+func ShowPostByAccount(username string) *[]db.FieldPost {
+
+	check := userModel.FindOne(username)
+
+	if check.Username == "" {
+		return nil
+	}
+
+	response := userModel.FindUserAndPost(username)
+
+	return response
+}
+
+func SearchAccount(username string) string {
+	check := userModel.FindOne(username)
+
+	if check.Username != ""{
+		return check.Username
+	}
+
+	return ""
+}
