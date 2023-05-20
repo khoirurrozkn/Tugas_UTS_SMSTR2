909e56d0689b0f3f3b6174dd0a13355993863bf9
FedoRas0420T <128302801+FedoRas0420T@users.noreply.github.com>
2023-05-20 20:20:38 +0700

    Update user.go
    Meng enkripsi password

--- controller/user/user.go
+++ controller/user/user.go
@@ -3,6 +3,7 @@ package userController
 import (
 	"utsstrukdat/db"
 	"utsstrukdat/model/user"
+	"utsstrukdat/config/crypt"
 )
 
 func Register(req db.FieldUser, verPassword string) int {
@@ -17,18 +18,22 @@ func Register(req db.FieldUser, verPassword string) int {
 		return 400
 	}
 
+	req.Password = crypt.Encode(req.Password)
+
 	userModel.Create(req)
+	
 	return 200
+	
 }
 
 func Login(req db.FieldUser) db.FieldUser {
 
 	check := userModel.FindOne(req.Username)
-
-	if check.Username == req.Username && check.Password == req.Password {
+	
+	if check.Username == req.Username && crypt.Decode(check.Password) == req.Password {
 		return check
 	}
-
+	check.Password = crypt.Encode(check.Password)
 	return db.FieldUser{}
 }
 
